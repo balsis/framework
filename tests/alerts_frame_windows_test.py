ca30ae331e091228e378a4f89b4d3573b0ff2e68
artur.balsis <artur.balsis@gmail.com>
2024-03-26 18:01:39 +0300

    fix naming

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -10,7 +10,7 @@ class TestBrowserWindow:
         result_text = new_tab_page.check_opened_tab()
         assert result_text == "This is a sample page"
     def test_new_window(self, driver):
-        new_tab_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
-        new_tab_page.open()
-        result_text = new_tab_page.check_opened_tab()
+        new_window_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+        new_window_page.open()
+        result_text = new_window_page.check_opened_tab()
         assert result_text == "This is a sample page"
