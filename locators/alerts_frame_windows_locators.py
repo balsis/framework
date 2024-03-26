9a23f87debd04c71d9891a7b99bf844ecac7905c
artur.balsis <artur.balsis@gmail.com>
2024-03-26 18:00:26 +0300

    add alerts frame

---
+++ locators/alerts_frame_windows_locators.py
@@ -0,0 +1,5 @@
+class BrowserWindowsPageLocators:
+    TAB_BUTTON = ("xpath", "//button[@id='tabButton']")
+    WINDOW_BUTTON = ("xpath", "//button[@id='windowButton']")
+
+    TITLE_NEW = ("xpath", "//h1[@id='sampleHeading']")
---
+++ tests/alerts_frame_windows_test.py
@@ -0,0 +1,16 @@
+import time
+
+from pages.alerts_frame_windows_page import BrowserWindowsPage
+
+
+class TestBrowserWindow:
+    def test_new_tab(self, driver):
+        new_tab_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+        new_tab_page.open()
+        result_text = new_tab_page.check_opened_tab()
+        assert result_text == "This is a sample page"
+    def test_new_window(self, driver):
+        new_tab_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+        new_tab_page.open()
+        result_text = new_tab_page.check_opened_tab()
+        assert result_text == "This is a sample page"
