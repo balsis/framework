f22b8d2baa7ba43c0c2871ab69a24751a670ac7a
artur.balsis <artur.balsis@gmail.com>
2024-04-10 18:11:48 +0300

    add ResizablePage

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -1,8 +1,10 @@
 class SortablePageLocators:
     TAB_LIST = ("xpath", "//a[@id='demo-tab-list']")
-    LIST_ITEM = ("xpath", "//div[@class='vertical-list-container mt-4']/div[@class='list-group-item list-group-item-action']")
+    LIST_ITEM = (
+    "xpath", "//div[@class='vertical-list-container mt-4']/div[@class='list-group-item list-group-item-action']")
     TAB_GRID = ("xpath", "//a[@id='demo-tab-grid']")
     GRID_ITEM = ("xpath", "//div[@class='create-grid']/div[@class='list-group-item list-group-item-action']")
+
 
 class SelectablePageLocators:
     TAB_LIST = ("xpath", "//a[@id='demo-tab-list']")
@@ -11,3 +13,10 @@ class SelectablePageLocators:
     TAB_GRID = ("xpath", "//a[@id='demo-tab-grid']")
     GRID_ITEM = ("xpath", "//li[@class='list-group-item list-group-item-action']")
     GRID_ITEM_ACTIVE = ("xpath", "//li[@class='list-group-item active list-group-item-action']")
+
+
+class ResizablePageLocators:
+    RESIZABLE_BOX_HANDLE = ("xpath", "//div[@id='resizableBoxWithRestriction']/span")
+    RESIZABLE_BOX = ("xpath", "//div[@id='resizableBoxWithRestriction']")
+    RESIZABLE_HANDLE = ("xpath", "//div[@id='resizable']/span")
+    RESIZABLE = ("xpath", "//div[@id='resizable']")
