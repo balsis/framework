2ea9ba2103df27d4ddb1bca59417ce59ef27b380
artur.balsis <artur.balsis@gmail.com>
2024-04-17 17:18:39 +0300

    allure steps for form_page and form_test

--- tests/form_test.py
+++ tests/form_test.py
@@ -1,12 +1,17 @@
 import time
 
 from pages.form_page import FormPage
+import allure
 
 
-class TestFormPage:
-    def test_form(self, driver):
-        form_page = FormPage(driver, "https://demoqa.com/automation-practice-form")
-        form_page.open()
-        person_info = form_page.fill_form_fields()
-        result = form_page.form_result()
-        assert [person_info.first_name, person_info.last_name, person_info.email] == [result[0].split()[0], result[0].split()[1],  result[1]]
+@allure.suite('Forms')
+class TestForm:
+    @allure.feature('FormPage')
+    class TestFormPage:
+        @allure.title('Check form')
+        def test_form(self, driver):
+            form_page = FormPage(driver, "https://demoqa.com/automation-practice-form")
+            form_page.open()
+            person_info = form_page.fill_form_fields()
+            result = form_page.form_result()
+            assert [person_info.first_name, person_info.last_name, person_info.email] == [result[0].split()[0], result[0].split()[1],  result[1]]
